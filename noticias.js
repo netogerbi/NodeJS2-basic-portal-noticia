5b2e9cf79eb659d0b01df49fcb844ac912837f48
Trix <sistema@trixsys.com.br>
2019-01-04 13:55:34 -0200

    Criando um servidor e definindo rotas e respostas para as rotas

---
+++ noticias.js
@@ -0,0 +1,14 @@
+const http = require('http');
+
+http.createServer((request,response) =>{
+    let categoria = request.url;
+    console.log("Categoria", categoria)
+    if(categoria == "/tecnologia"){
+        response.end("<html><body>Notícias de Tecnologia</body></html>");
+    }else if(categoria=="/moda"){
+        response.end("<html><body>Notícias de Moda</body></html>");
+    }else{
+        response.end("<html><body>Portal de Notícias</body></html>");
+    }
+
+}).listen(3010);

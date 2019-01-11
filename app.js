b421aade8ec2c7a6dad225755d6de1b0a32cb3f0
Trix <sistema@trixsys.com.br>
2019-01-11 13:01:56 -0200

    Iniciando servidor e organizando rotas com express

---
+++ app.js
@@ -0,0 +1,15 @@
+var express = require("express"); //retorna uma função
+var app = express(); //executa a função
+
+app.listen(3010, ()=>{
+    console.log("Servidor rodando com express");
+});
+
+app.get("/",(request, response)=>{
+    response.send("<html><body>Portal de Notícias</body></body></html>");
+});
+
+app.get("/tecnologia",(request,response)=>{
+    console.log(request);
+    response.send("<html><body>Notícias de Tecnologia</body></html>");
+});

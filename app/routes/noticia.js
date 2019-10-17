978f1cf2748a18bfde5881424ac70abc690a5a3e
Grupo5-Reculos Gerbi Neto <netogerbi2@hotmail.com>
2019-10-16 21:56:25 -0300

    connecting in mysql database and executing a query

--- app/routes/noticia.js
+++ app/routes/noticia.js
@@ -1,5 +1,21 @@
 module.exports = (app) => {
+
   app.get("/noticias",(req, res)=>{
-    res.render("noticias/noticias");
+    
+  const mysql = require('mysql')
+
+  const conn = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: '',
+    database: 'portal_noticias'
+  })
+
+  const query = 'SELECT * FROM noticias;'
+  conn.query(query, (error, result) => {
+    res.send(result)
+  })
+
+//    res.render("noticias/noticias");
   });
 }

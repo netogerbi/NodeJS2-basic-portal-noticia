a684ce2a4d1245f5f28d0877cbe7ee925f080c26
Grupo5-Reculos Gerbi Neto <netogerbi2@hotmail.com>
2019-10-18 22:00:01 -0300

    adding consign to includes routes automatically

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const app = require('./config/server')
-const rotasNoticias = require('./app/routes/noticia')(app)
-const rotasHome = require('./app/routes/home')(app)
-const rotasfrmNoticias = require('./app/routes/formulario-noticias')(app)
+
+// const rotasNoticias = require('./app/routes/noticia')(app)
+// const rotasHome = require('./app/routes/home')(app)
+// const rotasfrmNoticias = require('./app/routes/formulario-noticias')(app)
+
 const listener = app.listen(3010, ()=> console.log("Servidor rodando com express"));
 console.log("Porta: " + listener.address().port)
--- config/server.js
+++ config/server.js
@@ -1,6 +1,9 @@
-var express = require("express");
-var app = express();
+const express = require("express");
+const consign = require('consign');
+const app = express();
 app.set('view engine', 'ejs')
+
+consign().include('./app/routes').into(app)
 
 // set deve receber o caminha a partir da raiz, pois está a variavel app está sendo criada em app.js na raiz do projeto
 app.set('views','./app/views') 

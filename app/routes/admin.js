4e46a6bed9bc8c1f25c3e6e3f8472cb8aadeb68d
Grupo5-Reculos Gerbi Neto <netogerbi2@hotmail.com>
2019-10-19 14:46:29 -0300

    Input validation using the express-validator

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,6 +7,23 @@ module.exports = function (app) {
 
   app.post("/noticias/salvar", (req, res)=>{
     const noticia = req.body;
+
+    req.assert('titulo', 'O título é obgrigatório').notEmpty();
+    req.assert('resumo', 'O resumo é obgrigatório').notEmpty();
+    req.assert('titulo', 'O resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+    req.assert('autor', 'O autor é obgrigatório').notEmpty();
+    req.assert('data_noticia', 'A data é obgrigatória').notEmpty().isDate({fomat: 'YYYY-MM-DD'});
+    req.assert('noticia', 'A notícia é obgrigatória').notEmpty();
+
+    const errors = req.validationErrors();
+
+    if(errors) {
+      res.render("admin/form_add_noticia");
+      return;
+    
+    }
+
+
     const conn = app.config.dbConnection()
     const noticiasModel = new app.app.models.NoticiasDAO(conn);
 
